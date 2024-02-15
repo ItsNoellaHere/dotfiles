59fe4d35e1f8a3ed6748eeab04eebfb2a5e6a487
ItsNoellaHere <49508517+ItsNoellaHere@users.noreply.github.com>
2024-02-14 19:59:49 -0700

    created basic install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+XDG_CONFIG_HOME=$HOME/.config
+
+cp -r ./alacritty $XDG_CONFIG_HOME/alacritty
+cp -r ./nvim-conf $XDG_CONFIG_HOME/nvim
+cp -r ./tmux $XDG_CONFIG_HOME/tmux
+cp zsh/zshenv $HOME/.zshenv
+cp -r ./zsh $XDG_CONFIG_HOME/zsh
